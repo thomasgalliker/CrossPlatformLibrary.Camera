d511bdbf785398153a0df26a7297fcde757a8e22
thomasgalliker <thomas@bluewin.ch>
2016-12-14 19:35:55 +0100

    Remove old iOSUnified library

--- CrossPlatformLibrary.Camera.iOSUnified/LinkerHelper.cs
+++
@@ -1,7 +0,0 @@
-﻿using Foundation;
-
-// This assembly annotation is essential for CrossPlatformLibrary to work correctly in Xamarin.iOS and MonoTouch projects.
-// It prevents the Xamarin linker from removing 'unused' code.
-[assembly: Preserve(typeof(CrossPlatformLibrary.LinkerHook), AllMembers = true)]
-[assembly: Preserve(typeof(CrossPlatformLibrary.Camera.MediaAccess), AllMembers = true)]
-[assembly: Preserve(typeof(CrossPlatformLibrary.Camera.MediaLibrary), AllMembers = true)]
